4160691f6a241efd36498f972275a41180e3b234
newbiefpf <1326788346@qq.com>
2023-02-07 14:28:16 +0800

    配置文件

--- config/config.go
+++ config/config.go
@@ -1,14 +1,15 @@
 package config
 
 import (
-	"fmt"
 	"github.com/BurntSushi/toml"
 	"time"
 )
 
 type Config struct {
-	Title string
-	App   app
+	Title     string
+	App       app
+	MysqlLink mysqlLink
+	LogFile   logFile
 }
 
 type app struct {
@@ -17,6 +18,18 @@ type app struct {
 	Mark    string
 	Release time.Time
 }
+type mysqlLink struct {
+	Username string
+	Password string
+	Host     string
+	Port     int
+	Dbname   string
+	Timeout  string
+}
+type logFile struct {
+	LogRouterPath string `toml:"Log_ROUTER_PATH"`
+	FileName      string `toml:"File_Name"`
+}
 
 var ConfigToml Config
 
@@ -24,7 +37,4 @@ func ReadConfig() {
 	if _, err := toml.DecodeFile("./config/config.toml", &ConfigToml); err != nil {
 		panic(err)
 	}
-
-	fmt.Printf("App信息：%+v\n\n", ConfigToml.App)
-
 }
